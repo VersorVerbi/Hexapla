f15e0616db9e62ec2320a0bb9ea7fa27465d43fb
VersorVerbi <doroapodeus@gmail.com>
2019-10-21 16:29:26 -0500

    Create usfx_importer.php

---
+++ usfx_importer.php
@@ -0,0 +1,68 @@
+<?php
+
+function usfx_import($simpleXml) {
+  $lang = $simpleXml->languageCode; // either 2-letter ISO code or 3-letter Ethnologue code, plus optional dialect indicator
+  $books = $simpleXml->book;
+  foreach ($books as $book) {
+    $bookId = $book->id;
+    // if FRT or BAK, then it's front or back matter and should probably be skipped
+    // otherwise, it's the 3-letter ID for the book of the Bible (based on what source??)
+    // h = running header for top of page (e.g., "Matthew")
+    // cl = chapter label, e.g., "chapter," "psalm"
+    // p = all paragraphs of all kinds
+      // attr: level, sfm, style
+      // c, v, ve, milestone, ca, cp, table, f, cl, and also:
+      // qt = quoted text
+      // nd = name of Deity (i.e., the Tetragrammaton or its translation)
+      // tl = transliterated text
+      // va = alternate verse starting point/number
+      // vp = published verse starting point/number
+      // qs = "Selah"
+      // qac = first character of a line of acrostic poetry
+      // ef = extended footnote
+      // fm = footnote marker "not recommended for use," but we know how that goes
+      // x = cross-reference
+      // ex = extended cross-reference
+      // sls = passage based on secondary language source (e.g., Aramaic or Greek in the OT)
+      // dc = deuterocanonical bits
+      // bk = quoted book title
+      // pn = proper name
+      // k = keyword
+      // ord = ordinal number ending (-th)
+      // add = additional material (i.e., clarifying text in an otherwise literal translation)
+      // sig = signature of author of epistle
+      // bd, it, bdit, sc, no = formatting codes "not recommended for use"
+      // fig = figure suggestion for illustrations, maps, etc.
+      // ndx = index entry
+      // w = word attributes, e.g., lemma
+      // wh = Hebrew word list entry
+      // wg = Greek word list entry
+      // wr = wordlist/glossary/dictionary reference
+      // quoteStart, quoteRemind, quoteEnd = ???
+      // ior = ???
+      // wj = words of Jesus
+      // cs = character style
+      // fs = footnote hyperlink start ... ??
+      // wtp = indicates previous word was addressed plurally since English doesn't differentiate
+      // da = indicates addressed person is addressee (vocative indicator)
+      // optionalLineBreak, ref, rq, em, periph, xt, gw = ???
+      // zcr, zcb, zcg, zcy = color-coded text for color-coded Bibles
+      // otherwise, just straight text content
+    // q = poetry line "paragraph"
+    // milestone = marker... of... some kind
+    // mt = main/full title of the book
+    // d = descriptive title of a psalm (Hebrew title?)
+    // s = section heading (not canonical text)
+    // sectionBoundary = ???
+    // b = empty element for blank lines between stanzas of poetry ONLY
+    // c = chapter starting point
+    // ca = alternate chapter starting point
+    // toc = table of contents
+    // table
+    // periph = ???
+    // v = verse starting point (not the verse itself)
+    // ve = milestone marking end of a verse; content is comment
+    // cp = chapter starting point... again? synonymous with 'c'
+    // f = footnote
+  }
+}
